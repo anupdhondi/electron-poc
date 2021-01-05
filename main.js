19cc377202e76efe4ddb78727ea233186e7ea808
anupdhondi5 <anupdhondi5@gmail.com>
2021-01-05 16:26:06 +0530

    Redirect button to data from nodejs

---
+++ main.js
@@ -0,0 +1,51 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+// Set env
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "Redirect to Other App",
+    width: isDev ? 800 : 500,
+    height: 600,
+    resizable: isDev ? true : false,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile("./app/index.html");
+}
+
+app.on("ready", () => {
+  createMainWindow();
+  mainWindow.on("ready", () => (mainWindow = null));
+});
+
+ipcMain.on("redirect", e => {
+  mainWindow.loadURL("http://localhost:9090");
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
+app.allowRendererProcessReuse = true;
